85344320575309212975bce2fca5e0dd2a76099f
Temur Umarov <umarovtemur25@gmail.com>
2023-07-20 23:50:37 +0500

    bug

--- database.js
+++ database.js
@@ -6,6 +6,7 @@ module.exports = new Sequelize(
   process.env.DB_PASSWORD,
   {
     dialect: "postgres",
+    dialectModule: pg,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialectOptions: {
